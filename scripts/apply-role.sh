539133bff06781afd2d961af6346ef46d8750734
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-01-03 15:41:55 +0100

    Don't load kernel moduls

--- scripts/apply-role.sh
+++ scripts/apply-role.sh
@@ -1,1 +1,1 @@
-ansible localhost -c local -m include_role -a name=quamotion.device_cloud_node -e "preseed_docker_images=false"
+ansible localhost -c local -m include_role -a name=quamotion.device_cloud_node -e "preseed_docker_images=false load_kernel_modules=false"
