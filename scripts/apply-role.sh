48a203e6c79d7e7c9dd32e9e040673c2405b5cdb
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-01-10 16:27:58 +0100

    Specify distribution, release

--- scripts/apply-role.sh
+++ scripts/apply-role.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Apply the device-cloud-node role. This will install the binaries (Docker, Kubernetes, Helm,...)
-ansible localhost -c local -m include_role -a name=quamotion.device_cloud_node -e "preseed_docker_images=false load_kernel_modules=false"
+ansible localhost -c local -m include_role -a name=quamotion.device_cloud_node -e "preseed_docker_images=false load_kernel_modules=false ansible_distribution=Ubuntu ansible_distribution_release=bionic"
