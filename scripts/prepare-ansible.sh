fc5db0e4241a08fa584644db3b19c0e18413eedf
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-03-27 11:45:38 +0100

    Update to v0.2

--- scripts/prepare-ansible.sh
+++ scripts/prepare-ansible.sh
@@ -3,4 +3,4 @@ apt-get install -y software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt-get install -y ansible
 
-ansible-galaxy install quamotion.device_cloud_node
+ansible-galaxy install quamotion.device_cloud_node,v0.2
